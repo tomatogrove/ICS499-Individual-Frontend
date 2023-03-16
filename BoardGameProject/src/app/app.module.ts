7ab492439142b027997aa800e79085a4736b19c3
tomatogrove <66643214+tomatogrove@users.noreply.github.com>
2023-03-15 19:25:25 -0500

    basic component creation
    for home screen and game set up

--- BoardGameProject/src/app/app.module.ts
+++ BoardGameProject/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeScreenComponent } from './home-screen/home-screen.component';
+import { GameSetupComponent } from './game-setup/game-setup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeScreenComponent,
+    GameSetupComponent
   ],
   imports: [
     BrowserModule,
